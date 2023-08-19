ffd6223af8f4b253b952185272889271bcc1a386
Huraira Younas <raohuraira331.rb@gmail.com>
2023-08-20 01:45:53 +0500

    bugs fixed

--- src/consts/auth_consts.js
+++ src/consts/auth_consts.js
@@ -1,6 +1,6 @@
 const herokuAddr = "https://impulse-senpai-e6a2f49e21ab.herokuapp.com";
 const myAPI = "http://192.168.100.18:5000";
-const host = myAPI;
+const host = herokuAddr;
 
 export default host;
 export { myAPI, herokuAddr };
